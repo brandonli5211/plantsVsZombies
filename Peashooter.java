5372edafca71d6d900bf5fcd4b525a6a434fe605
brandonli5211 <103465318+brandonli5211@users.noreply.github.com>
2023-01-07 13:27:11 -0500

    added animation switch for peashooter

--- Peashooter.java
+++ Peashooter.java
@@ -9,12 +9,15 @@ public class Peashooter extends Plant {
 
     public Peashooter(GamePanel parent, int x, int y) {
         super(parent, x, y, 200);
-        shootTimer = new Timer(2300, (ActionEvent e) -> {
-            this.isShooting = true;
-            getGp().getPeaLanes().get(y).add(new Pea(getGp(), y, 103 + this.getX() * 100));
+        shootTimer = new Timer(2250, (ActionEvent e) -> {
+            if(getGp().getActiveZombies().get(y).size() > 0){
+                this.isShooting = true;
+                getGp().getPeaLanes().get(y).add(new Pea(getGp(), y, 103 + this.getX() * 100));
+            }else{
+                this.isShooting = false;
+            }
         });
         shootTimer.start();
-        this.isShooting = false;
     }
 
     public boolean isShooting(){
